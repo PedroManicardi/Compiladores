07f97d401cd7b750a652c3a12392a0f804d2bb73
Gabriel Ribeiro Rodrigues Dessotti <83930010+dessotti1@users.noreply.github.com>
2024-06-23 18:30:22 -0300

    token att

--- src/token.h
+++ src/token.h
@@ -3,35 +3,57 @@
 
 // Tipos de token
 typedef enum {
-    CONST, VAR, PROCEDURE, IDENTIFIER, NUMBER, 
-    PLUS, MINUS, TIMES, DIVIDE, ASSIGN, LPAREN, RPAREN,
-    SEMICOLON,
-    BEGIN, END, IF, THEN, WHILE, DO, CALL, ODD,
-    EQ, LT, LTE, GT, GTE,
-    END_OF_PROGRAM, ERROR,
+  CONST,
+  VAR,
+  PROCEDURE,
+  IDENTIFIER,
+  NUMBER,
+  PLUS,
+  MINUS,
+  TIMES,
+  DIVIDE,
+  ASSIGN,
+  LPAREN,
+  RPAREN,
+  SEMICOLON,
+  BEGIN,
+  END,
+  IF,
+  THEN,
+  WHILE,
+  DO,
+  CALL,
+  ODD,
+  EQ,
+  LT,
+  LTE,
+  GT,
+  GTE,
+  END_OF_PROGRAM,
+  ERROR,
 } TokenType;
 
 // Struct do token
 typedef struct {
-    // Tipo do token
-    TokenType type;
+  // Tipo do token
+  TokenType type;
 
-    // Lexema do token
-    char lexeme[100];
+  // Lexema do token
+  char lexeme[100];
 } Token;
 
 // Struct do par token-classe
 typedef struct {
-    // Token
-    Token token;
+  // Token
+  Token token;
 
-    // Classe associada ao token
-    char classe[100];
+  // Classe associada ao token
+  char classe[100];
 
-    // Numero da linha
-    int line_number;
+  // Numero da linha
+  int line_number;
 } TokenClassPair;
 
-TokenClassPair getNextTokenClass(FILE *file, int *pos, int *line_number);
+// TokenClassPair getNextTokenClass(FILE *file, int *pos, int *line_number);
 
 #endif // TOKEN_H
